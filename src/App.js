25db023bb5a77105ceffb4cd97e89a9231286803
sameerapk <sameerakmurthy@gmail.com>
2021-06-03 19:52:23 +0530

    importing forms properly

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
 import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
+import Form from './../src/components/Form/Form'
 import { getPosts } from './actions/posts';
 import useStyles from './styles';
 import memories from './images/memories.png';
