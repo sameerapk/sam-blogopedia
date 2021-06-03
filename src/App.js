a31548283802f3fcea541e29b6f0a31b10a8433f
sameerapk <sameerakmurthy@gmail.com>
2021-06-03 09:20:26 +0530

    added ui for the post

--- src/App.js
+++ src/App.js
@@ -20,11 +20,11 @@ const App = () => {
             </AppBar>
             <Grow in>
                 <Container>
-                    <Grid container justify='space-between' alignItems='stretch' spacing={3}>
-                        <Grid item xs={12} sm={7}>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12} sm={8}>
                             <Posts />
                         </Grid>
-                        <Grid item xs={12} sm={5}>
+                        <Grid item xs={12} sm={4}>
                             <Form />
                         </Grid>
                     </Grid>
--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import {useSelector} from 'react-redux'
+import {Grid, CircularProgress} from '@material-ui/core'
 import useStyles from './styles'
 import Post from './post/Post'
 
 const Posts = () => {
     const posts = useSelector((state)=> state.posts)
     const classes = useStyles()
-    console.log('here it is', posts)
     return (
-        <>
-            <h1>collection of posts</h1>
-            <Post />
-            <Post />
-        </>
+        !posts.length ? null : (
+            <Grid className={classes.container} container alignItems='stretch' spacing={3}>
+                {
+                    posts.map((post)=> (
+                        <Grid key={post._id} item xs={12} sm={4} md={4}>
+                           <Post post={post} /> 
+                        </Grid>
+                    ))
+                }
+            </Grid>
+        )
     )
 }
 
